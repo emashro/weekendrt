86cc637524109b25a38c9a298c4abf3308103f18
emashro <emashro@gmail.com>
2018-09-05 23:39:06 +0900

    add ray class

---
+++ ray.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "vec3.h"
+
+class ray {
+public:
+	ray() {}
+	ray(const vec3 &a, const vec3 &b) : a(a), b(b) {}
+
+	const vec3 &origin() const { return a; }
+	const vec3 &direction() const { return b; }
+	vec3 point_at_parameter(float t) const { return a + t * b; }
+
+	vec3 a; // origin
+	vec3 b; // direction
+};
