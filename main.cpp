872ce2f46727e3c511e90f46c2ebb3831b73bff1
emashro <emashro@gmail.com>
2018-09-07 01:38:40 +0900

    add camera, add anti-aliasing

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "sphere.h"
 #include "hitable.h"
 #include "hitablelist.h"
+#include "camera.h"
 
 vec3 color(const ray &r, hitable *world) {
 	hit_record rec;
@@ -21,23 +22,23 @@ int main()
 {
 	int nx = 200;
 	int ny = 100;
+	int ns = 100;
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-	vec3 lower_left_corner(-2.0f, -1.0f, -1.0f);
-	vec3 horizontal(4.0f, 0.0f, 0.0f);
-	vec3 vertical(0.0f, 2.0f, 0.0f);
-	vec3 origin(0.0f, 0.0f, 0.0f);
 	hitable *list[2];
 	list[0] = new sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f);
 	list[1] = new sphere(vec3(0, -100.5f, -1.0f), 100.0f);
 	hitable *world = new hitable_list(list, 2);
+	camera cam;
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
-			float u = float(i) / float(nx);
-			float v = float(j) / float(ny);
-			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-
-			vec3 p = r.point_at_parameter(2.0f);
-			vec3 col = color(r, world);
+			vec3 col(0.0f, 0.0f, 0.0f);
+			for (int s = 0; s < ns; s++) {
+				float u = float(i + drand48()) / float(nx);
+				float v = float(j + drand48()) / float(ny);
+				ray r = cam.get_ray(u, v);
+				col += color(r, world);
+			}
+			col /= float(ns);
 			int ir = int(255.99f * col[0]);
 			int ig = int(255.99f * col[1]);
 			int ib = int(255.99f * col[2]);
