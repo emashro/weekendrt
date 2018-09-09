77e2f41003712fd2623be415152863096f5e6d27
emashro <emashro@gmail.com>
2018-09-09 22:48:48 +0900

    move camera

--- camera.h
+++ camera.h
@@ -5,14 +5,20 @@
 
 class camera {
 public:
-	camera() {
-		lower_left_corner = vec3(-2.0f, -1.0f, -1.0f);
-		horizontal = vec3(4.0f, 0.0f, 0.0f);
-		vertical = vec3(0.0f, 2.0f, 0.0f);
-		origin = vec3(0.0f, 0.0f, 0.0f);
+	camera(const vec3 &lookfrom, const vec3 &lookat, const vec3 &vup, float vfov, float aspect) {
+		float theta = vfov * M_PI / 180.0f;
+		float half_height = tan(theta / 2.0f);
+		float half_width = aspect * half_height;
+		origin = lookfrom;
+		vec3 w = unit_vector(lookfrom - lookat);
+		vec3 u = unit_vector(cross(vup, w));
+		vec3 v = cross(w, u);
+		lower_left_corner = origin - half_width * u - half_height * v - w;
+		horizontal = 2.0f * half_width * u;
+		vertical = 2.0f * half_height * v;
 	}
-	ray get_ray(float u, float v) {
-		return ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+	ray get_ray(float s, float t) {
+		return ray(origin, lower_left_corner + s * horizontal + t * vertical - origin);
 	}
 	vec3 origin;
 	vec3 lower_left_corner;
