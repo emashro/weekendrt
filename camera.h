a8e6822fab9d74ef4c6abb84063d6d48cbf64f4e
emashro <emashro@gmail.com>
2018-09-10 00:57:51 +0900

    add defocus blur

--- camera.h
+++ camera.h
@@ -3,25 +3,39 @@
 #include "vec3.h"
 #include "ray.h"
 
+vec3 random_in_unit_disk()
+{
+	vec3 p;
+	do {
+		p = 2.0f * vec3(drand48(), drand48(), 0) - vec3(1.0f, 1.0f, 0.0f);
+	} while (dot(p, p) >= 1.0f);
+	return p;
+}
+
 class camera {
 public:
-	camera(const vec3 &lookfrom, const vec3 &lookat, const vec3 &vup, float vfov, float aspect) {
+	camera(const vec3 &lookfrom, const vec3 &lookat, const vec3 &vup, float vfov, float aspect, float aperture, float focus_dist) {
+		lens_radius = aperture / 2.0f;
 		float theta = vfov * M_PI / 180.0f;
 		float half_height = tan(theta / 2.0f);
 		float half_width = aspect * half_height;
 		origin = lookfrom;
-		vec3 w = unit_vector(lookfrom - lookat);
-		vec3 u = unit_vector(cross(vup, w));
-		vec3 v = cross(w, u);
-		lower_left_corner = origin - half_width * u - half_height * v - w;
-		horizontal = 2.0f * half_width * u;
-		vertical = 2.0f * half_height * v;
+		w = unit_vector(lookfrom - lookat);
+		u = unit_vector(cross(vup, w));
+		v = cross(w, u);
+		lower_left_corner = origin - half_width * focus_dist * u - half_height * focus_dist * v - focus_dist * w;
+		horizontal = 2.0f * half_width * focus_dist * u;
+		vertical = 2.0f * half_height * focus_dist * v;
 	}
 	ray get_ray(float s, float t) {
-		return ray(origin, lower_left_corner + s * horizontal + t * vertical - origin);
+		vec3 rd = lens_radius * random_in_unit_disk();
+		vec3 offset = u * rd.x() + v * rd.y();
+		return ray(origin + offset, lower_left_corner + s * horizontal + t * vertical - origin - offset);
 	}
 	vec3 origin;
 	vec3 lower_left_corner;
 	vec3 horizontal;
 	vec3 vertical;
+	vec3 u, v, w;
+	float lens_radius;
 };
